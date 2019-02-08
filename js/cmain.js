b22579f46ebbee4171d2a06106a2489aff2c6a02
zedbpro <zedblqpam@gmail.com>
2019-02-08 23:06:01 +0100

    nn
    nn

---
+++ js/cmain.js
@@ -0,0 +1,47 @@
+var click1 = document.getElementById('zed1');
+
+var click2 = document.getElementById('zed2');
+
+var click3 = document.getElementById('zed3');
+
+var click4 = document.getElementById('zed4');
+
+var click5 = document.getElementById('zed5');
+
+
+var youtube = document.getElementById('lol');
+
+
+click2.addEventListener("click",function() {
+
+  youtube.src = "https://www.youtube.com/embed/Vyumt7LZL-8";
+
+
+});
+click1.addEventListener("click",function() {
+
+  youtube.src = "https://www.youtube.com/embed/N0h1X2Kua7E";
+
+
+});
+click3.addEventListener("click",function() {
+
+  youtube.src = "https://www.youtube.com/embed/RKBplWbWt8g";
+  /*https://www.youtube.com/embed/CwE-haZR2fc*/
+
+
+});
+click4.addEventListener("click",function() {
+
+  youtube.src = "https://www.youtube.com/embed/HSUZeu06XBc";
+
+
+});
+click5.addEventListener("click",function() {
+
+  youtube.src = "https://www.youtube.com/embed/O1KcFdlB_8Q";
+
+
+});
+//"https://www.youtube.com/embed/N0h1X2Kua7E"
+//"https://www.youtube.com/embed/Vyumt7LZL-8"
